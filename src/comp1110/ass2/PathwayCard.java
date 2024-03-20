4880c6a6a27bc18d0bc190e1d7e2bdf68f50c207
u7897414 <u7897414@anu.edu.au>
2024-03-20 22:55:52 +0800

    Have set the PathwayCard.java class.

---
+++ src/comp1110/ass2/PathwayCard.java
@@ -0,0 +1,42 @@
+package comp1110.ass2;
+
+/**
+ * The PathwayCard class represents a pathway card in the game.
+ * It includes information about the position where the card is placed and methods for combining pathway cards.
+ */
+public class PathwayCard {
+
+    private int[][] layout; // Represents the layout of the pathway card
+
+    /**
+     * Constructs a PathwayCard object with the specified layout.
+     * @param layout the layout of the pathway card represented as a 2D array
+     */
+    public PathwayCard(int[][] layout) {
+        this.layout = layout;
+    }
+
+    /**
+     * Rotates the pathway card clockwise by 90 degrees.
+     */
+    public void rotateClockwise() {
+        // Implementation to rotate the pathway card clockwise
+    }
+
+    /**
+     * Flips the pathway card horizontally.
+     */
+    public void flipHorizontal() {
+        // Implementation to flip the pathway card horizontally
+    }
+
+    /**
+     * Flips the pathway card vertically.
+     */
+    public void flipVertical() {
+        // Implementation to flip the pathway card vertically
+    }
+
+    // Other methods can be added as needed
+
+}
