44066d2da39acb07d6b9f6c4cd4281e445a9a960
u7897414 <u7897414@anu.edu.au>
2024-03-20 22:39:21 +0800

    Have set the Card.java class and State.java class.

---
+++ src/comp1110/ass2/Card.java
@@ -0,0 +1,21 @@
+package comp1110.ass2;
+
+/**
+ * The Card class represents a card in the game.
+ * It contains information about the type of card and any other relevant properties.
+ */
+public class Card {
+
+    private String type; // The type of card
+
+    /**
+     * Constructs a Card object with the specified type.
+     * @param type the type of card
+     */
+    public Card(String type) {
+        this.type = type;
+    }
+
+    // Getter and setter for type (for example:a pathway card, fire tile card, or any other type of card)
+
+}
