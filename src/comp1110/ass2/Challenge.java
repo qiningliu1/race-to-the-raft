40e59459ff5ca946db0df40edeffdb965f0f4ada
Ishaan <u7598889@anu.edu.au>
2024-03-21 09:47:24 +1100

    added some classes and documentation.

--- src/comp1110/ass2/Challenge.java
+++ src/comp1110/ass2/Challenge.java
@@ -1,4 +1,5 @@
 package comp1110.ass2;
+import java.util.Random;
 /**
  * Challenges define the initial layout of the island,
  * the location of raft, location of cats and the spread of fire.
@@ -9,8 +10,21 @@ public class Challenge {
      **/
     private final int challengeNumber;
 
-
+    /**
+     * The constructor is incomplete, as we need other aspects
+     * like the Island for each challenge number, board size etc.,
+     * to make up a challenge.
+     * This is a very rudimentary method.
+     **/
     public Challenge(int challengeNumber) {
         this.challengeNumber = challengeNumber;
     }
+
+    /**
+     * @return The initial island for this challenge
+     */
+    public Island getIsland(int challengeNumber) {
+        return null;
+    }
+
 }
