c5b09a5f2bfdbfb96c01eee909671ad41c82320b
u7897414 <u7897414@anu.edu.au>
2024-03-20 21:51:59 +0800

    Have set the Raft.java class.

---
+++ src/comp1110/ass2/Raft.java
@@ -0,0 +1,30 @@
+package comp1110.ass2;
+
+/**
+ * The Raft class represents the raft in the game.
+ * It includes the location of the raft on the island and methods for landing cats on the raft.
+ */
+public class Raft {
+
+    private Location location; // Represents the location of the raft on the island
+
+    /**
+     * Constructs a Raft object with the specified location.
+     * @param location the location of the raft on the island
+     */
+    public Raft(Location location) {
+        this.location = location;
+    }
+
+    /**
+     * Lands a cat on the raft.
+     * @param cat the cat to be landed on the raft
+     */
+    public void landCat(Cat cat) {
+        // Implementation to land a cat on the raft
+    }
+
+    // Other methods can be added as needed
+
+}
+
