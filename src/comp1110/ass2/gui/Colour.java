71bd84a21faa60098007cacddebfe6816331e451
Ishaan <u7598889@anu.edu.au>
2024-03-20 22:51:45 +1100

    added some classes and documentation.

--- src/comp1110/ass2/gui/Colour.java
+++
@@ -1,9 +0,0 @@
-package comp1110.ass2.gui;
-
-public class Colour {
-    //This is very initial design
-    public char toChar(){
-      return 'b';//this sentence is not important just make the grammer work
-    };
-
-}
--- src/comp1110/ass2/gui/Orientation.java
+++
@@ -1,5 +0,0 @@
-package comp1110.ass2.gui;
-
-public class Orientation {
-    //This class defiened different orientation for different cards
-}
