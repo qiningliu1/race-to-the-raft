0ddcb1be5dfab0f3cf1339d829722a2b5e962e44
u7897414 <u7897414@anu.edu.au>
2024-03-20 21:48:06 +0800

    Have set the FireTile.java class.

---
+++ src/comp1110/ass2/FireTile.java
@@ -0,0 +1,28 @@
+package comp1110.ass2;
+
+/**
+ * The FireTile class represents a fire tile in the game.
+ * It includes the location of the fire tile on the island and methods for placing the fire tile.
+ */
+public class FireTile {
+
+    private Location location; // Represents the location of the fire tile on the island
+
+    /**
+     * Constructs a FireTile object with the specified location.
+     * @param location the location of the fire tile on the island
+     */
+    public FireTile(Location location) {
+        this.location = location;
+    }
+
+    /**
+     * Places the fire tile on the island at the specified position.
+     */
+    public void placeFire() {
+        // Implementation to place the fire tile on the island
+    }
+
+    // Other methods can be added as needed
+
+}
