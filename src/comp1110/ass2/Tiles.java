10eef73861e5173c17ad35d277a9e03702cbb174
u7897414 <u7897414@anu.edu.au>
2024-04-22 16:34:18 +0800

    tiles class added

--- src/comp1110/ass2/Tiles.java
+++ src/comp1110/ass2/Tiles.java
@@ -6,7 +6,41 @@ package comp1110.ass2;
  * required for the game to work.
  */
 public enum Tiles {
+    BLUE, GREEN, RED, YELLOW, PURPLE, WILD, Fire, Object, None;
 
+    public static Tiles fromChar(char tiles) {
+        return switch (tiles) {
+            case 'b' -> BLUE;
+            case 'g' -> GREEN;
+            case 'r' -> RED;
+            case 'y' -> YELLOW;
+            case 'p' -> PURPLE;
+            case 'f' -> Fire;
+            case 'w' -> WILD;
+            case 'o' -> Object;
+            default -> None;
+        };
+    }
+
+    /**
+     * convert the tile into a char that symbolises it. For example,
+     * RED can be symbolised by 'r'.
+     *
+     * @return char representing the colour/tile.
+     */
+    public char toChar() {
+        return switch (this) {
+            case BLUE -> 'b';
+            case GREEN -> 'g';
+            case RED -> 'r';
+            case YELLOW -> 'y';
+            case PURPLE -> 'p';
+            case Fire -> 'f';
+            case WILD -> 'w';
+            case Object -> 'o';
+            default -> 'x';
+        };
+    }
 }
 
 
