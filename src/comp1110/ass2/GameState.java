3eaac46f3991756468ab0cba23bcbcfff5b24502
Qining Liu <u7100555@anu.edu.au>
2024-04-29 00:17:01 +1000

    Implement GameState Class and other class init

--- src/comp1110/ass2/GameState.java
+++ src/comp1110/ass2/GameState.java
@@ -1,8 +1,45 @@
 package comp1110.ass2;
+import comp1110.ass2.gui.Game;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+
 public class GameState {
+    String [] gameState;
+    private Board board;
+    private Decks[][] decks;
+    private ExhaustedCat[] ExCats;
+    private FireTiles[][] FireBag;
+
+public GameState(Board board,Decks[][] decks, ExhaustedCat[] ExCats,FireTiles[][] FireBag){
+    this.board= board;
+    this.decks = decks;
+    this.ExCats = ExCats;
+    this.FireBag = FireBag;
+}
+
+public String[] gameState (){
+
+
+
+
+
+    return null;
+}
+
+
+
+public Object[] getGameState()
+    {
+        Object[] gameState = new Object[4];
+        gameState[0] = board;
+        gameState[1] = decks;
+        gameState[2] = ExCats;
+        gameState[3] = FireBag;
+        return gameState;
+
+    }
 
 }
 
