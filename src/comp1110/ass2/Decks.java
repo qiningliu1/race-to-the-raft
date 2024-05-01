94703bea0fc8255b353fd96ff095c7638ce052fe
Ishaan <u7598889@anu.edu.au>
2024-05-01 20:16:42 +1000

    comments added

--- src/comp1110/ass2/Decks.java
+++ src/comp1110/ass2/Decks.java
@@ -8,10 +8,16 @@ import java.util.ArrayList;
 public class Decks {
     private ArrayList<SingleDeck> fulldeck; //decks object is a list of all decks.
 
+    /**
+     * all possible deck ids
+     */
     public enum DECK_ID {
         A, B, C, D;
     }
 
+    /**
+     * These are the string representation of full decks.
+     */
     static String[] deckA = Utility.DECK_A;
     static String[] deckB = Utility.DECK_B;
     static String[] deckC = Utility.DECK_C;
@@ -57,6 +63,12 @@ public class Decks {
 
     }
 
+    /**
+     * takes in a char id for a specific card in a deck, and gives us the index at which that card exists in the
+     * respective deck.
+     * @param c card id between a-y
+     * @return index associated.
+     */
     public static int cardIDtoIndex(char c) {
         int index = 0;
         index = switch (c) {
@@ -90,7 +102,11 @@ public class Decks {
         return index;
     }
 
-
+    /**
+     * takes in a deckString like AabBCdefD, and returns all the singleDecks that exists in that combined deck.
+     * @param deckString string given in string representation.
+     * @return ArrayList of singleDeck obejcts.
+     */
     public ArrayList<SingleDeck> getSingleDecks(String deckString) {
         ArrayList<SingleDeck> req = new ArrayList<>(4);
 
@@ -111,6 +127,13 @@ public class Decks {
     /**
      * Author: Ishaan Kapoor u7598889
      **/
+
+
+    /**
+     * This nested class is designed to make and store individual decks like DECK_A and so on.
+     * This class takes in a simple string like Aabc and converts it into an object with ID A and retrieves cards
+     * a,b and c from Utility, and stores them in a ArrayList.
+     */
     class SingleDeck {
         private Decks.DECK_ID ID;
         private ArrayList<Cards> cards;
@@ -125,6 +148,11 @@ public class Decks {
             return required;
         }
 
+        public SingleDeck (DECK_ID ID, ArrayList<Cards> cards) {
+            this.ID = ID;
+            this.cards = cards;
+        }
+
 
         public SingleDeck(String singleDeck) { // singleDeck like Aabc
             this.ID = charToID(singleDeck.charAt(0));
@@ -135,6 +163,11 @@ public class Decks {
             return cards;
         }
 
+        /**
+         * since we will retreive the ID from a string, we first need to convert it into a DECK_ID object.
+         * @param c char which represents the ID
+         * @return DECK_ID value based on that char.
+         */
         public static Decks.DECK_ID charToID(char c) {
             Decks.DECK_ID ID = null;
             ID = switch (c) {
